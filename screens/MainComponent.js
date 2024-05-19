ca490e2231ee2c55918e3497c819d0568df1304e
DrBecky22 <101670044+DrBecky22@users.noreply.github.com>
2024-05-19 17:10:57 -0400

    committ
    week 1 fixes plus weekend workshop task 1 and 2.  Issues with final partners card render in step 3

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -2,11 +2,13 @@
 import { Platform, View } from 'react-native';
 import Constants from 'expo-constants';
 // import { CAMPSITES } from '../shared/campsites';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import HomeScreen from './HomeScreen';
 import CampsiteInfoScreen from './CampsiteInfoScreen';
 import DirectoryScreen from './DirectoryScreen';
-import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './HomeScreen';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import AboutScreen from './AboutScreen';    
+import ContactScreen from './ContactScreen';
 
 const Drawer = createDrawerNavigator();
 
@@ -46,32 +48,79 @@ const DirectoryNavigator = () => {
                 name='CampsiteInfo'
                 component={CampsiteInfoScreen}
                 options={({ route }) => ({
-                    title: route.params.campsite.name
+                title: route.params.campsite.name
                 })}
             />
+            <Stack.Screen
+                    name='About'
+                    component={AboutScreen}
+            />
+            <Stack.Screen
+                name='Contact'
+                component={ContactScreen}
+                options={{ title: 'Contact Us' }}
+            />
+            
         </Stack.Navigator>
     )
-}
+};
+
+const AboutNavigator = () => {
+    const Stack = createStackNavigator();
+    return (
+        <Stack.Navigator
+            screenOptions={screenOptions}
+        >
+            <Stack.Screen
+                name='About'
+                component={AboutScreen}
+            />
+        </Stack.Navigator>
+    );
+};
+
+const ContactNavigator = () => {
+    const Stack = createStackNavigator();
+    return (
+        <Stack.Navigator
+            screenOptions={screenOptions}
+        >
+            <Stack.Screen
+                name='Contact'
+                component={ContactScreen}
+                options={{ title: 'Contact Us' }}
+            />
+        </Stack.Navigator>
+    );
+};
 
 const Main = () => {
 
     return (
         <View style={{ flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight }}>
             <Drawer.Navigator
-                initialRouteName="HomeDrawer"
-                screenOptions={{ drawerStyle={ backgroundColor: '#CEC8FF' }}}
+                initialRouteName="Home"
+                screenOptions={{ drawerStyle: { backgroundColor: '#CEC8FF' }}}
             >
                 <Drawer.Screen
                     name="Home"
                     component={HomeNavigator}
                     options={{ title: 'Home' }}
                 />
-                 <Drawer.Screen
+                <Drawer.Screen
                     name="Directory"
                     component={DirectoryNavigator}
                     options={{ title: 'Directory' }}
                 />
-
+                <Drawer.Screen
+                    name="About"
+                    component={AboutNavigator}
+                />
+                <Drawer.Screen
+                    name="Contact"
+                    component={ContactNavigator}
+                    options={{ title: 'Contact Us' }}
+                />
             </Drawer.Navigator>
         </View>
     );
