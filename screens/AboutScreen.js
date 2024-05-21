839026504118a486cdeeaa025577aab3251041a6
DrBecky22 <101670044+DrBecky22@users.noreply.github.com>
2024-05-20 21:54:39 -0400

    commit
    weekend 1 final

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,23 +1,22 @@
-import { Text } from "react-native";
-import { ScrollView } from "react-native-gesture-handler"
+import { Text, ScrollView } from "react-native";
 import { useState } from "react";
 import { Avatar, Card, ListItem } from "react-native-elements";
 import { PARTNERS } from "../shared/partners";
 
+const Mission = () => {
+    return (
+        <Card>
+            <Card.Title>Our Mission</Card.Title>
+            <Card.Divider/>
+            <Text style='margin:10'>We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.</Text>
+        </Card>
+    );
+};
 
 const AboutScreen = () => {
-    const [partners, setpartners] = useState();
+    const [partners, setPartners] = useState(PARTNERS);
     // the instructions did not say what this piece of state should be managing, nor what to initialize it to
 
-    const Mission = () => {
-        return (
-            <Card>
-                <Card.Title>Our Mission</Card.Title>
-                <Card.Divider/>
-                <Text style='margin:10'>We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.</Text>
-            </Card>
-        );
-    };
 
     return (
         <ScrollView>
@@ -25,7 +24,7 @@ const AboutScreen = () => {
             <Card>
                 <Card.Title>Community Partners</Card.Title>
                 <Card.Divider/>
-                {PARTNERS.map(partner) => {
+                {partners.map(partner => {
                     return (
                         <ListItem key={partner.id}>
                             <Avatar source={partner.image} style='rounded'/>
@@ -35,8 +34,8 @@ const AboutScreen = () => {
                             </ListItem.Content>
                         </ListItem>
                     )
-                }}
-            <Card />
+                }) }
+            </Card>
             
         </ScrollView>
     );
